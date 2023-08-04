b11199382eabf58528d3e3139e3b66367947c40b
miroswd <altamirsantos93@hotmail.com>
2023-08-04 14:41:25 -0300

    Revert "feat: get envs with fromEnv method"
    This reverts commit 58f0efceec9d678d9f8db0c90393f64dd795aa29.

--- src/database/redis.js
+++ src/database/redis.js
@@ -1,5 +1,13 @@
 const { Redis } = require('@upstash/redis');
 
-const redisConnection = Redis.fromEnv();
+const {
+  UPSTASH_REDIS_REST_URL,
+  UPSTASH_REDIS_REST_TOKEN,
+} = process.env;
+
+const redisConnection = new Redis({
+  url: UPSTASH_REDIS_REST_URL,
+  token: UPSTASH_REDIS_REST_TOKEN,
+});
 
 module.exports = { redisConnection };
